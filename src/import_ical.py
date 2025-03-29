3f222be9b65dda6140f4893b70af1f27d41ed3cc
prometheus42 <christian@overcoded.dev>
2025-03-29 14:13:15 +0100

    Refactor functions to methods.

--- src/import_ical.py
+++ src/import_ical.py
@@ -28,87 +28,6 @@ from com.sun.star.task import XJobExecutor
 from com.sun.star.ui.dialogs.TemplateDescription import FILEOPEN_SIMPLE
 
 
-def fill_table(ctx, filename):
-    """Fills the first worksheet with data from an iCalendar file.
-
-    All events from a given iCalendar file are imported. Every column
-    represents a attribute from the file and has a corrsponding header. The
-    cells will be configured with a number format depending on the type of data
-    of that column (datetime, timedelta, string, etc.)."""
-    smgr = ctx.ServiceManager
-    desktop = smgr.createInstanceWithContext("com.sun.star.frame.Desktop", ctx)
-    doc = desktop.getCurrentComponent()
-    sheet = doc.getCurrentController().getActiveSheet()
-    # write attributes into table header
-    attr = ['name', 'begin', 'end', 'duration', 'uid', 'description', 'created',
-            'last_modified', 'location', 'url', 'transparent', 'alarms',
-            'attendees', 'categories', 'status', 'organizer', 'classification']
-    for column, name in enumerate(attr):
-        cell = sheet.getCellByPosition(column, 0)
-        cell.String = name
-    # iterate over all events and add data to table
-    for row, e in enumerate(read_ical_file(filename)):
-        for column, name in enumerate(attr):
-            cell = sheet.getCellByPosition(column, row+1)
-            fill_cell_with_data(doc, getattr(e, name), cell)
-    # mark all columns and set them to optimal width
-    selection = sheet.getCellRangeByPosition(0, 1, len(attr)-1, 1)
-    for c in selection.Columns:
-        c.OptimalWidth = True
-
-
-def read_ical_file(filename):
-    """Reads an iCalendar file and returns all events from that file."""
-    with open(filename, 'r') as calendar_file:
-        c = Calendar(calendar_file.read())
-    return c.events
-
-
-def fill_cell_with_data(doc, data, cell):
-    """Fills a single cell with given data.
-
-    Before the data is stored in the cell of an worksheet, the type of the data
-    is checked. All necessary conversion and type casts are done before the
-    value of the cell is set."""
-    number_format = doc.getNumberFormats()
-    local_settings = Locale()
-    local_settings.Language = 'de'
-    if not data:
-        cell.String = ''
-    elif isinstance(data, str):
-        cell.String = data
-    elif isinstance(data, Arrow):
-        # write datetime into cell
-        cell.String = data.format('DD.MM.YYYY HH:mm:ss')
-        # set number format for cell
-        format_string = 'TT.MM.JJJJ HH:MM:SS'
-        number_format_id = number_format.queryKey(
-            format_string, local_settings, True)
-        # check whether a value was found for the requested format (-1 == no value)
-        if number_format_id == -1:
-            number_format_id = number_format.addNew(
-                format_string, local_settings)
-        cell.NumberFormat = number_format_id
-    elif isinstance(data, timedelta):
-        # write datetime into cell
-        cell.String = str(data)
-        # set number format for cell
-        format_string = 'HH:MM:SS'
-        number_format_id = number_format.queryKey(
-            format_string, local_settings, True)
-        # check whether a value was found for the requested format (-1 == no value)
-        if number_format_id == -1:
-            number_format_id = number_format.addNew(
-                format_string, local_settings)
-        cell.NumberFormat = number_format_id
-    elif isinstance(data, set):
-        cell.String = ', '.join([x for x in data])
-    else:
-        print('Unsupported type: {}'.format(type(data)))
-        # return data without conversion to trigger an exception
-        return data
-
-
 class ImportButton(unohelper.Base, XJobExecutor):
     """Handles a click on the menu item for importing an iCalendar file.
 
@@ -160,8 +79,8 @@ class ImportButton(unohelper.Base, XJobExecutor):
 
         file_dialog = smgr.createInstanceWithContext('com.sun.star.ui.dialogs.FilePicker', self.ctx)
         file_dialog.initialize((FILEOPEN_SIMPLE,))
-        file_dialog.appendFilter('iCalendar-Datei (.ics)', '*.ics')
-        file_dialog.setTitle('Lade iCalendar-Datei')
+        file_dialog.appendFilter('iCalendar file (.ics)', '*.ics')
+        file_dialog.setTitle('Open iCalendar file')
         file_dialog.setMultiSelectionMode(False)
         ok = file_dialog.execute()
         if ok:
@@ -169,7 +88,7 @@ class ImportButton(unohelper.Base, XJobExecutor):
             file_dialog.dispose()
             try:
                 # TODO: Allow multiple files to be imported into a single worksheet.
-                fill_table(self.ctx, uno.fileUrlToSystemPath(list_of_files[0]))
+                self.fill_table(self.ctx, uno.fileUrlToSystemPath(list_of_files[0]))
             except UnicodeDecodeError as e:
                 show_message_box(self.ctx, 'Error', 'Unicode error while reading file.')
                 self.logger.error(e)
@@ -183,6 +102,84 @@ class ImportButton(unohelper.Base, XJobExecutor):
                 show_message_box(self.ctx, 'Error', 'Calendar file not valid.')
                 self.logger.error(e)
             show_message_box(self.ctx, 'Imported calender', 'Calendar file was successfully imported.')
+
+    def fill_table(self, ctx, filename):
+        """Fills the first worksheet with data from an iCalendar file.
+
+        All events from a given iCalendar file are imported. Every column
+        represents a attribute from the file and has a corrsponding header. The
+        cells will be configured with a number format depending on the type of data
+        of that column (datetime, timedelta, string, etc.)."""
+        smgr = ctx.ServiceManager
+        desktop = smgr.createInstanceWithContext("com.sun.star.frame.Desktop", ctx)
+        doc = desktop.getCurrentComponent()
+        sheet = doc.getCurrentController().getActiveSheet()
+        # write attributes into table header
+        attr = ['name', 'begin', 'end', 'duration', 'uid', 'description', 'created',
+                'last_modified', 'location', 'url', 'transparent', 'alarms',
+                'attendees', 'categories', 'status', 'organizer', 'classification']
+        for column, name in enumerate(attr):
+            cell = sheet.getCellByPosition(column, 0)
+            cell.String = name
+        # iterate over all events and add data to table
+        for row, e in enumerate(self.read_ical_file(filename)):
+            for column, name in enumerate(attr):
+                cell = sheet.getCellByPosition(column, row+1)
+                self.fill_cell_with_data(doc, getattr(e, name), cell)
+        # mark all columns and set them to optimal width
+        selection = sheet.getCellRangeByPosition(0, 1, len(attr)-1, 1)
+        for c in selection.Columns:
+            c.OptimalWidth = True
+
+    def read_ical_file(self, filename):
+        """Reads an iCalendar file and returns all events from that file."""
+        with open(filename, 'r') as calendar_file:
+            c = Calendar(calendar_file.read())
+        return c.events
+
+    def fill_cell_with_data(self, doc, data, cell):
+        """Fills a single cell with given data.
+
+        Before the data is stored in the cell of an worksheet, the type of the data
+        is checked. All necessary conversion and type casts are done before the
+        value of the cell is set."""
+        number_format = doc.getNumberFormats()
+        local_settings = Locale()
+        local_settings.Language = 'de'
+        if not data:
+            cell.String = ''
+        elif isinstance(data, str):
+            cell.String = data
+        elif isinstance(data, Arrow):
+            # write datetime into cell
+            cell.String = data.format('DD.MM.YYYY HH:mm:ss')
+            # set number format for cell
+            format_string = 'TT.MM.JJJJ HH:MM:SS'
+            number_format_id = number_format.queryKey(
+                format_string, local_settings, True)
+            # check whether a value was found for the requested format (-1 == no value)
+            if number_format_id == -1:
+                number_format_id = number_format.addNew(
+                    format_string, local_settings)
+            cell.NumberFormat = number_format_id
+        elif isinstance(data, timedelta):
+            # write datetime into cell
+            cell.String = str(data)
+            # set number format for cell
+            format_string = 'HH:MM:SS'
+            number_format_id = number_format.queryKey(
+                format_string, local_settings, True)
+            # check whether a value was found for the requested format (-1 == no value)
+            if number_format_id == -1:
+                number_format_id = number_format.addNew(
+                    format_string, local_settings)
+            cell.NumberFormat = number_format_id
+        elif isinstance(data, set):
+            cell.String = ', '.join([x for x in data])
+        else:
+            print('Unsupported type: {}'.format(type(data)))
+            # return data without conversion to trigger an exception
+            return data
 
 
 def show_message_box(ctx, title, message):
