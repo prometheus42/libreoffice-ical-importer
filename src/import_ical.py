6495c209f3cbdad439b4ff045e1e1139d4329255
prometheus42 <christian@overcoded.dev>
2025-03-30 14:54:13 +0200

    Make UTF8 explicit and handle errors better.

--- src/import_ical.py
+++ src/import_ical.py
@@ -47,7 +47,7 @@ class IcalImporter(unohelper.Base, XJobExecutor):
         This code is inspired by the LibreOffice extension "Code Highligher 2"
         licensed under the GNU General Public License version 3."""
         self.logger = logging.getLogger('import_ical')
-        formatter = logging.Formatter("%(asctime)s %(levelname)s [%(funcName)s::%(lineno)d] %(message)s")
+        formatter = logging.Formatter('%(asctime)s %(levelname)s [%(funcName)s::%(lineno)d] %(message)s')
         self.logger.setLevel(logging.DEBUG)
         console_handler = logging.StreamHandler()
         console_handler.setFormatter(formatter)
@@ -55,9 +55,9 @@ class IcalImporter(unohelper.Base, XJobExecutor):
         try:
             # log file should be stored in the user directory
             userpath = uno.getComponentContext().ServiceManager.createInstance(
-                            "com.sun.star.util.PathSubstitution").substituteVariables("$(user)", True)
+                            'com.sun.star.util.PathSubstitution').substituteVariables('$(user)', True)
             logfile = Path(uno.fileUrlToSystemPath(userpath)) / 'import_ical.log'
-            file_handler = logging.FileHandler(logfile, mode="a", delay=True)
+            file_handler = logging.FileHandler(logfile, mode='a', delay=True)
             file_handler.setLevel(logging.DEBUG)
             file_handler.setFormatter(formatter)
             self.logger.addHandler(file_handler)
@@ -71,7 +71,7 @@ class IcalImporter(unohelper.Base, XJobExecutor):
         This code is inspired by the LibreOffice extension "Code Highligher 2"
         licensed under the GNU General Public License version 3."""
         # get information about extension and it's location
-        pip = self.ctx.getByName("/singletons/com.sun.star.deployment.PackageInformationProvider")
+        pip = self.ctx.getByName('/singletons/com.sun.star.deployment.PackageInformationProvider')
         self.logger.debug(f'Extensions: {pip.getExtensionList()}')
         extension_path = pip.getPackageLocation('de.ichmann.libreoffice.import_ical')
         self.logger.debug(f'Extension path: {extension_path}')
@@ -105,18 +105,19 @@ class IcalImporter(unohelper.Base, XJobExecutor):
             try:
                 self.fill_table(self.ctx, uno.fileUrlToSystemPath(list_of_files[0]))
             except UnicodeDecodeError as e:
-                show_message_box(self.ctx, _('Error'), _('Unicode error while reading file.'))
+                show_message_box(self.ctx, _('Error'), _('Calendar file has an invalid character endoding,\nshould be Unicode UTF-8.'))
                 self.logger.error(e)
             except RuntimeException as e:
                 show_message_box(self.ctx, _('Error'), _('An UNO error occured.'))
                 self.logger.error(e)
             except NotImplementedError as e:
-                show_message_box(self.ctx, _('Error'), _('File contains multiple calendars. This is not yet supported.'))
+                show_message_box(self.ctx, _('Error'), _('File contains multiple calendars.\nThis is not yet supported.'))
                 self.logger.error(e)
             except ParseError as e:
-                show_message_box(self.ctx, _('Error'), _('Calendar file not valid.'))
+                show_message_box(self.ctx, _('Error'), _('Calendar file is not valid.'))
                 self.logger.error(e)
-            show_message_box(self.ctx, _('Calender imported'), _('Calendar file was successfully imported.'))
+            else:
+                show_message_box(self.ctx, _('Calender imported'), _('Calendar file was successfully imported.'))
 
     def fill_table(self, ctx, filename):
         """Fills the first worksheet with data from an iCalendar file.
@@ -148,7 +149,7 @@ class IcalImporter(unohelper.Base, XJobExecutor):
 
     def read_ical_file(self, filename):
         """Reads an iCalendar file and returns all events from that file."""
-        with open(filename, 'r') as calendar_file:
+        with open(filename, 'r', encoding='utf-8', errors='replace') as calendar_file:
             c = Calendar(calendar_file.read())
         return c.events
 
@@ -205,7 +206,7 @@ def show_message_box(ctx, title, message):
     message_box = msgbox.MsgBox(ctx)
     message_box.addButton('OK')
     message_box.renderFromBoxSize(150)
-    message_box.numberOflines = 2
+    message_box.numberOflines = 3
     message_box.show(message, 0, title)
 
 
@@ -234,14 +235,14 @@ if __name__ == '__main__':
     # wait until LibreOffice starts and connection is established
     while ctx == None:
         try:
-            ctx = resolver.resolve("uno:socket,host=localhost,port=2002;urp;StarOffice.ComponentContext")
+            ctx = resolver.resolve('uno:socket,host=localhost,port=2002;urp;StarOffice.ComponentContext')
         except Exception as e:
             pass
 
-    print("Testing IcalImporter...")
+    print('Testing IcalImporter...')
 
     # trigger our job
     testjob = IcalImporter(ctx)
     testjob.trigger(())
 
-    print("IcalImporter tested.")
+    print('IcalImporter tested.')
